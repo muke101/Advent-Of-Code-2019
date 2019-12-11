0668769464bd9e101a567f51c5286c201fe00636
muke <muke101@protonmail.com>
2019-12-11 05:22:26 +0000

    large progress made on day 3

--- day3-1.scala
+++ day3-1.scala
@@ -12,20 +12,14 @@ object Main {
     
   }
 
-  def plotCoords(grid: Map[List[Int], Int], wire: Array[String], pos: List[Int]): Map[List[Int], Int] = {
+  def plotCoords(grid: Map[List[Int], Int], wire: Array[String], start: List[Int]): Map[List[Int], Int] = {
     if (wire.length > 0)  {
-      val nextCoord = pos.zip(parseMove(wire(0))).map{case (a,b) => a+b}
-
-      if (pos(0) == nextCoord(0)) {
-        val start = math.min(pos(1), nextCoord(1))
-        val end = math.max(pos(1), nextCoord(1))
-        return plotCoords(draw(grid, start, end, pos(0), "x"), wire.slice(1,wire.length), nextCoord) 
+      val end = start.zip(parseMove(wire(0))).map{case (a,b) => a+b}
+      List(start,end) match {
+        case List(List(x1,y1),List(x2,y2)) if x1 == x2 => drawY(grid,y1,y2,x1)
+        case List(List(x1,y1),List(x2,y2)) if y1 == y2 => drawX(grid,x1,x2,y1)
       }
-      else { //abusing the fact that I know in the context one of these will always be satisfied in the intended way - scala forces a catch all case for returning
-        val start = math.min(pos(0), nextCoord(0))
-        val end = math.max(pos(0), nextCoord(0))
-        return plotCoords(draw(grid, start, end, pos(1), "y"), wire.slice(1,wire.length), nextCoord) 
-      }
+      return plotCoords(grid, wire.slice(1,wire.length), end)
     }
     else  {
       return grid
@@ -44,29 +38,28 @@ object Main {
     }
   }
 
-  def draw(grid: Map[List[Int], Int], start: Int, end: Int, linePos: Int, axis: String): Map[List[Int], Int] =  {
-    if (start <= end)  {
-      if (axis == "x")  {
-        grid(List(linePos,start)) += 1 
-      }
-      if (axis == "y")  {
-        grid(List(start,linePos)) += 1
-      }
-      return draw(grid, start+1, end, linePos, axis)
-    }
-    else  {
-    if (axis == "x")  { //such a dirty hack but fuck it man I wanna move onto the next one
-      grid(List(linePos,end)) -= 1 
-    }
-    if (axis == "y")  {
-      grid(List(end,linePos)) -= 1
-    }
-      return grid
-    }
+  def drawX(grid: Map[List[Int], Int], start: Int, end: Int, linePos: Int) = {
+        val range = (start, end) match  {
+          case (x1,x2) if x1 < x2 => (start to end)
+          case (x1,x2) if x1 > x2 => (end to start)
+          case _ => (start to end)
+        }
+        range.toList.slice(1,range.length-1).map(x => grid.put(List(x,linePos),grid.getOrElseUpdate(List(x,linePos),0)+1))
+  }
+
+  def drawY(grid: Map[List[Int], Int], start: Int, end: Int, linePos: Int) = {
+        val range = (start, end) match  {
+          case (y1,y2) if y1 < y2 => (start to end)
+          case (y1,y2) if y1 > y2 => (end to start)
+          case _ => (start to end)
+        }
+        range.toList.slice(1,range.length-1).map(y => grid.put(List(linePos,y),grid.getOrElseUpdate(List(linePos,y),0)+1))
   }
 
   def shortestDistance(grid: Map[List[Int], Int]): Int =  {
     grid(List(0,0)) = 0 //also a dirty hack
-    return grid.keys.filter(grid(_) == 2).map((x,y) => math.abs(x) + math.abs(y)).min
+    val keys = grid.keys.filter(grid(_) == 2)
+    val absolute_keys = keys.map(_.map(math.abs(_)))
+    return absolute_keys.map(_.sum).min
   }
 }
