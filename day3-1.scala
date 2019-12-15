22c97fa11cc01f2de5a3ed6002d2ba0b3b3bf597
muke <muke101@protonmail.com>
2019-12-15 07:40:13 +0000

    completed day 3 part 1

--- day3-1.scala
+++ day3-1.scala
@@ -1,25 +1,24 @@
 import scala.io.Source
 import scala.math
-import scala.collection.mutable.Map
+import scala.collection.mutable.{Map,Set}
 
 object Main {
   def main(args: Array[String]): Unit = {
     val wires = Source.fromFile(args(0)).getLines
     val wire1 = wires.next.split(",")
     val wire2 = wires.next.split(",")
-    var grid = Map[List[Int], Int]().withDefaultValue(0)
-    println(shortestDistance(plotCoords(plotCoords(grid,wire1,List(0,0)), wire2, List(0,0))))
-    
+    var grid = Map[List[Int], Set[String]]()
+    println(shortestDistance(plotCoords(plotCoords(grid,wire1,"l1",List(0,0)),wire2,"l2",List(0,0))))
   }
 
-  def plotCoords(grid: Map[List[Int], Int], wire: Array[String], start: List[Int]): Map[List[Int], Int] = {
+  def plotCoords(grid: Map[List[Int], Set[String]], wire: Array[String], number: String, start: List[Int]): Map[List[Int], Set[String]] = {
     if (wire.length > 0)  {
       val end = start.zip(parseMove(wire(0))).map{case (a,b) => a+b}
       List(start,end) match {
-        case List(List(x1,y1),List(x2,y2)) if x1 == x2 => drawY(grid,y1,y2,x1)
-        case List(List(x1,y1),List(x2,y2)) if y1 == y2 => drawX(grid,x1,x2,y1)
+        case List(List(x1,y1),List(x2,y2)) if x1 == x2 => drawY(grid,y1,y2,x1,number)
+        case List(List(x1,y1),List(x2,y2)) if y1 == y2 => drawX(grid,x1,x2,y1,number)
       }
-      return plotCoords(grid, wire.slice(1,wire.length), end)
+      return plotCoords(grid, wire.slice(1,wire.length), number, end)
     }
     else  {
       return grid
@@ -38,27 +37,41 @@ object Main {
     }
   }
 
-  def drawX(grid: Map[List[Int], Int], start: Int, end: Int, linePos: Int) = {
+  def drawX(grid: Map[List[Int], Set[String]], start: Int, end: Int, linePos: Int, number: String) = {
         val range = (start, end) match  {
           case (x1,x2) if x1 < x2 => (start to end)
           case (x1,x2) if x1 > x2 => (end to start)
           case _ => (start to end)
         }
-        range.toList.slice(1,range.length-1).map(x => grid.put(List(x,linePos),grid.getOrElseUpdate(List(x,linePos),0)+1))
+        range.toList.slice(1,range.length-1).map{x => 
+          if (grid.isDefinedAt(List(x,linePos))) {
+            grid(List(x,linePos)) += number 
+          }
+          else  {
+            grid(List(x,linePos)) = Set(number)
+          }
+        }
   }
 
-  def drawY(grid: Map[List[Int], Int], start: Int, end: Int, linePos: Int) = {
+  def drawY(grid: Map[List[Int], Set[String]], start: Int, end: Int, linePos: Int, number: String) = {
         val range = (start, end) match  {
           case (y1,y2) if y1 < y2 => (start to end)
           case (y1,y2) if y1 > y2 => (end to start)
           case _ => (start to end)
         }
-        range.toList.slice(1,range.length-1).map(y => grid.put(List(linePos,y),grid.getOrElseUpdate(List(linePos,y),0)+1))
+        range.toList.slice(1,range.length-1).map{y => 
+          if (grid.isDefinedAt(List(linePos,y))) {
+            grid(List(linePos,y)) += number 
+          }
+          else  {
+            grid(List(linePos,y)) = Set(number)
+          }
+        }
   }
 
-  def shortestDistance(grid: Map[List[Int], Int]): Int =  {
-    grid(List(0,0)) = 0 //also a dirty hack
-    val keys = grid.keys.filter(grid(_) == 2)
+  def shortestDistance(grid: Map[List[Int], Set[String]]): Int =  {
+    //grid(List(0,0)) = 
+    val keys = grid.keys.filter(grid(_).size > 1)
     val absolute_keys = keys.map(_.map(math.abs(_)))
     return absolute_keys.map(_.sum).min
   }
